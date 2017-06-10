2139d182c6d3a18fbe8996eec39cacb87ea50195
szymski <szamon@wp.pl>
2017-06-10 12:29:47 +0200

    implemented changes in portfolio section and added media query

--- js/code.js
+++ js/code.js
@@ -2,17 +2,22 @@
 
 $(document).ready(function(){
 	$(".menu").click(function(){
-		if($(".menu").hasClass("menuOpen")){
+		if($(".overlay").hasClass("overlayOpen")){
 			$(".menu").removeClass("menuOpen");
-		}else
+			$(".overlay").removeClass("overlayOpen");
+		}else{
 			$(".menu").addClass("menuOpen");
-			});
-	});
+			$(".overlay").addClass("overlayOpen");
+			};
+	})});
 
 $(document).ready(function(){
-	$('a').click(
+	$('.scrollText a, .overlay li').click(
 		function(evt){
 			evt.preventDefault();
 			$("html, body").animate({"scrollTop": $($.attr(this, "href")).offset().top}, 500);
+			if($(".overlay").hasClass("overlayOpen")){
+			$(".menu").removeClass("menuOpen");
+			$(".overlay").removeClass("overlayOpen");}
 		});
 });
