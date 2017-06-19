c1aa11889255cf8fa7f004eb36ace74f1dc395a0
szymski <szamon@wp.pl>
2017-06-19 17:33:25 +0200

    adding skills carousel

--- js/code.js
+++ js/code.js
@@ -21,3 +21,28 @@ $(document).ready(function(){
 			$(".overlay").removeClass("overlayOpen");}
 		});
 });
+
+function slideStart(){
+	var slides = $(".slides");
+	var counter = 1;
+	var interval = setInterval(function(){
+		var slider = function(){
+			slides.animate({"margin-left":"-=240px"}, 1000, function(){
+				if(counter < slides.length){
+				counter +=1;
+				console.log("okrążenie="+counter);
+				$(".progressBar img:nth-of-type("+counter+")").addClass("active");
+				$(".progressBar img:nth-of-type("+(counter-1)+")").removeClass("active");
+			}else{
+				counter = 1;
+				$(".progressBar img:nth-of-type("+slides.length+")").removeClass("active");
+			}
+			});}
+			slider();
+	}, 3000);
+}
+
+
+
+slideStart();
+
